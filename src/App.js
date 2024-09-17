d0b2fa0c93142a348b385d82e58c690d85e777b4
LongHaiTown <huynhvanthanit@gmail.com>
2024-09-17 13:23:53 +0700

    MMM

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='outer-app-title'>
+        <div className='app-title'>
+          <h1 id="appTitle">Staying Productive~</h1>
+          <h1 id="appTitle_shadow">Staying Productive~</h1>
+        </div>
+        <div className='someGreeting'>
+          <p style={{paddingLeft:"auto"}}>hi</p>
+        </div>
+      </div>
+
+
+      <div className='scheduleBoard'>
+      </div>
+
+      <div className='todo-things'>
+        <div className='control-panel'>
+
+        </div>
+        <div className='tpplist'>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
